b4e9402d4531746021b0f2e5de26c5e4cd82bab6
gabrieljimenezfg <gabrieljimenez.fg@gmail.com>
2026-01-28 11:26:17 +0100

    rename for clarity

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -3,38 +3,38 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    private int healthPotions;
-    private int manaPotions;
-    private HealingPotionSO healingPotion;
-    private ManaPotionSO manaPotion;
+    private int healthPotionsAmountHeld;
+    private int manaPotionsAmountHeld;
+    private HealingPotionSO healingPotionData;
+    private ManaPotionSO manaPotionData;
 
     public bool PickupHealingPotion()
     {
-        if (healthPotions <= 0) return false;
+        if (healthPotionsAmountHeld <= 0) return false;
 
-        healthPotions--;
+        healthPotionsAmountHeld--;
         return true;
     }
 
     public float GetHealingPotionHealthAmount()
     {
-        return healingPotion.healingAmount;
+        return healingPotionData.healingAmount;
     }
     
     public float GetManaPotionRecoverAmount()
     {
-        return healingPotion.healingAmount;
+        return healingPotionData.healingAmount;
     }
 
     public void Save(ref InventoryState inventoryState)
     {
-        inventoryState.healthPotions = healthPotions;
-        inventoryState.manaPotions = manaPotions;
+        inventoryState.healthPotions = healthPotionsAmountHeld;
+        inventoryState.manaPotions = manaPotionsAmountHeld;
     }
 
     public void Load(InventoryState inventoryState)
     {
-        healthPotions = inventoryState.healthPotions;
-        manaPotions = inventoryState.manaPotions;
+        healthPotionsAmountHeld = inventoryState.healthPotions;
+        manaPotionsAmountHeld = inventoryState.manaPotions;
     }
 }
