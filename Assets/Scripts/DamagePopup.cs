a6a83eb33c87bb3d04a38ae891432310c030d328
Your Name <gabrieljimenez.fg@gmail.com>
2026-02-05 09:52:15 +0100

    fix damage number rotation

--- Assets/Scripts/DamagePopup.cs
+++ Assets/Scripts/DamagePopup.cs
@@ -6,10 +6,8 @@ public class DamagePopup : MonoBehaviour
 {
     public static DamagePopup Create(Vector3 position, float damageAmount)
     {
-        var yOffset = Vector3.up * 2f;
-        var positionOffset = position + yOffset;
-        var damagePopupTransform = Instantiate(GameAssets.i.damagePopupPrefab, positionOffset, Quaternion.identity);        
-        damagePopupTransform.LookAt(Camera.main.transform);
+        var damagePopupTransform = Instantiate(GameAssets.i.damagePopupPrefab, position, Quaternion.identity);        
+        damagePopupTransform.rotation = Camera.main.transform.rotation;
         var damagePopup = damagePopupTransform.GetComponent<DamagePopup>();
         damagePopup.Setup(damageAmount);
 
@@ -23,7 +21,6 @@ public class DamagePopup : MonoBehaviour
     [SerializeField] private float disappearTimerMax = 1f;
     private float disappearTimer;
     [SerializeField] private float moveYSpeed = 5f;
-    [SerializeField] private float yOffset = 5f;
     private Color textColor;
 
     private void Awake()
@@ -34,8 +31,8 @@ public class DamagePopup : MonoBehaviour
 
     private void Update()
     {
-        transform.rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position);
-        transform.position += new Vector3(0, moveYSpeed) * Time.deltaTime;
+        transform.rotation = Camera.main.transform.rotation;
+        transform.position += transform.up * (moveYSpeed * Time.deltaTime);
         
         disappearTimer -= Time.deltaTime;
         if (disappearTimer > disappearTimerMax * .5f)
