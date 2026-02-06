77fcf27e4f50aa1d09bc0999925d113341de41de
gabrieljimenezfg <gabrieljimenez.fg@gmail.com>
2026-02-06 14:13:29 +0100

    spawn corpse on enemy death

--- Assets/Scripts/Corpse.cs
+++ Assets/Scripts/Corpse.cs
@@ -1,8 +1,15 @@
+using System;
 using UnityEngine;
 
 public class Corpse : MonoBehaviour
 {
     [SerializeField] private GameObject minionPrefab;
+    [SerializeField] private float aliveTime;
+
+    private void Start()
+    {
+        Destroy(gameObject, aliveTime);
+    }
 
     public void SpawnMinion()
     {
