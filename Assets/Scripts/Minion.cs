7d4831db993f8e0927b25ea9bddb4919c344a2fe
Your Name <gabrieljimenez.fg@gmail.com>
2026-02-07 11:48:09 +0100

    polish logic for minions to disengage and go to player when above max range

--- Assets/Scripts/Minion.cs
+++ Assets/Scripts/Minion.cs
@@ -13,28 +13,56 @@ public class Minion : MonoBehaviour
     private float targetUpdateTimer;
 
     [SerializeField] private float damage;
-
     [SerializeField] private float detectionRadius;
+    [SerializeField] private float maxDistanceFromPlayer;
+    private float resumeCombatDistance;
+    private bool isReturningToPlayer;
 
     private void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        /*
+         * if we have a detection radius of 10 meters, and a max distance from player of 15 meters
+         * when we go above 15, we want to go back to the player, but to avoid being stuck in a state
+         * of back and forth between chasing and going to the player, we make sure we go back to at least enough
+         * distance so that the detection radius wont detect the enemy
+         * this is why we require detection radius to be smaller than max distance from player
+         */
+        resumeCombatDistance = maxDistanceFromPlayer - detectionRadius;
     }
 
     private void Update()
     {
         attackingCooldownTimer -= Time.deltaTime;
         targetUpdateTimer -= Time.deltaTime;
+        
+        var distanceToPlayerSqr = (transform.position - Player.Instance.transform.position).sqrMagnitude;
 
-        HandleTargetUpdating();
-
-        if (!enemyTarget)
+        if (distanceToPlayerSqr > maxDistanceFromPlayer * maxDistanceFromPlayer)
         {
+            isReturningToPlayer = true;
+        } else if (distanceToPlayerSqr < resumeCombatDistance * resumeCombatDistance)
+        {
+            isReturningToPlayer = false;
+        }
+
+        if (isReturningToPlayer)
+        {
+            enemyTarget = null;
             StayNearPlayer();
         }
         else
         {
-            ChaseAndAttackTarget();
+            HandleTargetUpdating();
+
+            if (!enemyTarget)
+            {
+                StayNearPlayer();
+            }
+            else
+            {
+                ChaseAndAttackTarget();
+            }
         }
     }
 
@@ -128,5 +156,28 @@ public class Minion : MonoBehaviour
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, detectionRadius);
+    }
+    
+    private void OnValidate()
+    {
+        if (detectionRadius >= maxDistanceFromPlayer)
+        {
+            Debug.LogWarning($"detectionRadius ({detectionRadius}) debe ser menor que maxDistanceFromPlayer ({maxDistanceFromPlayer})");
+            detectionRadius = maxDistanceFromPlayer - 1f;
+        }
+    }
+    
+    // call it a the end of Update() to visualize distances
+    private void DebugDrawLeash()
+    {
+        Vector3 playerPos = Player.Instance.transform.position;
+    
+        // Line to player
+        DebugUtils.DrawLine(transform.position, playerPos, isReturningToPlayer ? Color.red : Color.white);
+    
+        // Draw circles (approximation with lines)
+        DebugUtils.DrawCircle(playerPos, maxDistanceFromPlayer, Color.red);
+        DebugUtils.DrawCircle(playerPos, resumeCombatDistance, Color.green);
+        DebugUtils.DrawCircle(transform.position, detectionRadius, Color.yellow);
     }
 }
