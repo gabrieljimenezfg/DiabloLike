5a554b306253813dbdb83cc60a32a8149bf96df8
barrancofg <alvarobarranco.fg@gmail.com>
2026-02-12 16:30:04 +0100

    Archer (but can't revive)

---
+++ Assets/Scripts/ArcherEnemy.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ArcherEnemy : Enemy
+{
+    [SerializeField] private float reviveArea;
+
+    void Start()
+    {
+        base.Start();
+    }
+
+    void Update()
+    {
+        base.Update();
+    }
+}
