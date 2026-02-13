b2cc035779da81ea80be97c11299d0217463f854
barrancofg <alvarobarranco.fg@gmail.com>
2026-02-13 12:00:09 +0100

    MeshRenderer turn into Renderer, Started to revive archer logic

--- Assets/Scripts/ArcherEnemy.cs
+++ Assets/Scripts/ArcherEnemy.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class ArcherEnemy : Enemy
 {
@@ -12,5 +13,38 @@ public class ArcherEnemy : Enemy
     void Update()
     {
         base.Update();
+    }
+
+    //hay que cambiar esto, de momento indica el enemigo más cercano pero queremos que pase una lista de todos los enemigos dentro del area
+    private List<Enemy> FindEnemies()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, reviveArea);
+
+        List<Enemy> inAreaEnemies = new List<Enemy>();
+
+        foreach (Collider col in colliders)
+        {
+            if (col.TryGetComponent<Enemy>(out var enemy))
+            {
+                inAreaEnemies.Add(enemy);
+            }
+        }
+        return inAreaEnemies;
+    }
+
+    private List<Enemy> FindCorpses()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, reviveArea);
+
+        List<Enemy> inAreaEnemies = new List<Enemy>();
+
+        foreach (Collider col in colliders)
+        {
+            if (col.gameObject.layer.ToString() == "Corpse")
+            {
+                //inAreaEnemies.Add(enemy);
+            }
+        }
+        return inAreaEnemies;
     }
 }
