948e91ad37be0c600df999784e0e88fd80a66652
gabrieljimenezfg <gabrieljimenez.fg@gmail.com>
2026-02-23 10:51:53 +0100

    blast vfx

--- Assets/Scripts/Skills/AcidBlastSkill.cs
+++ Assets/Scripts/Skills/AcidBlastSkill.cs
@@ -13,11 +13,12 @@ public class AcidBlastSkill : BaseAreaDamageSkill
     private BoxCollider blastHitbox;
     private Player player;
     private int minionsConsumed;
+    private bool blasting;
 
     protected override void Awake()
     {
-        base.Awake();
         blastHitbox = GetComponent<BoxCollider>();
+        blastHitbox.enabled = false;
     }
 
     private void ApplySizeAndDamage()
@@ -25,8 +26,8 @@ public class AcidBlastSkill : BaseAreaDamageSkill
         var zOffset = blastRange * 0.5f;
         var sizeIncrease = perMinionWidthIncrease * minionsConsumed;
         var xySize = blastBaseWidth + sizeIncrease;
-        visual.localScale = new Vector3(xySize, xySize, blastRange);
-        visual.localPosition = new Vector3(visual.localPosition.x, visual.localPosition.y, zOffset);
+        // visual.localScale = new Vector3(xySize, xySize, blastRange);
+        // visual.localPosition = new Vector3(visual.localPosition.x, visual.localPosition.y, zOffset);
         blastHitbox.size = new Vector3(xySize, xySize, blastRange);
         blastHitbox.center = new Vector3(blastHitbox.center.x, blastHitbox.center.y, zOffset);
 
@@ -36,7 +37,7 @@ public class AcidBlastSkill : BaseAreaDamageSkill
 
     public override void StartCast()
     {
-        base.StartCast();
+        blastHitbox.enabled = true;
         ApplySizeAndDamage();
     }
 
